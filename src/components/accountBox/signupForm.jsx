7b86e56f623591838473bdbbe4a4a4cdeaf6a0d1
Pablo <paablo.92@hotmail.es>
2022-07-13 13:00:48 -0300

    descarto primer validacion

--- src/components/accountBox/signupForm.jsx
+++ src/components/accountBox/signupForm.jsx
@@ -9,6 +9,8 @@ import {
 } from "./common";
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
+import { handleChange, error, message } from "./validate";
+
 
 export function SignupForm(props) {
   const { switchToCreatePin } = useContext(AccountContext);
@@ -17,14 +19,24 @@ export function SignupForm(props) {
     <BoxContainer>
       <FormContainer>
         <Input type="text" placeholder="Full Name" />
-        <Input type="email" placeholder="Email" />
+        <Input type="email" id="email" placeholder="Email" />
+        <div>
+          <input
+            id="message"
+            name="message"
+            value={message}
+            onChange={handleChange}
+          />
+
+          {error && <h2 style={{ color: 'red' }}>{error}</h2>}
+        </div>
         <Input type="email" placeholder="Confirm Email" />
-        
+
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
       <SubmitButton type="submit" onClick={switchToCreatePin}>Signup</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
-      
+
     </BoxContainer>
   );
 }
